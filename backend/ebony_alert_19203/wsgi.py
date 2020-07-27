4ca64ccfccba18e16a31abe6c60e805830c64b99
Crowdbotics <team@crowdbotics.com>
2020-07-27 01:35:32 +0000

    Initial commit.

---
+++ backend/ebony_alert_19203/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ebony_alert_19203 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ebony_alert_19203.settings')
+
+application = get_wsgi_application()
